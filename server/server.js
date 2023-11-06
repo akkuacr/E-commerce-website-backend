1bf798e39b3e900453bde9f46d6397e96940bc93
Akash Chand <111640882+akkuacr@users.noreply.github.com>
2023-11-07 02:26:34 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,9 +2,20 @@
 config();
 import app from './app.js';
 import connectionToDB from './config/dbConnection.js';
+import cloudinary from 'cloudinary';
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5002;
+
+// Cloudinary configuration
+cloudinary.v2.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+  });
+
+
+
 
 
 
