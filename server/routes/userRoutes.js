62560aeb3f132a2c55a567c3649bd7b1499ddc24
Akash Chand <111640882+akkuacr@users.noreply.github.com>
2023-11-07 02:24:10 +0530

    Update userRoutes.js
    Routing for change password,update Profile done,login,logout done

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,14 +1,28 @@
 import { Router } from "express";
-import { register,login,logout,getProfile } from "../controllers/userController.js";
+import { register,login,logout,getProfile,forgotPassword,resetPassword,changePassword } from "../controllers/userController.js";
 import { isLoggedIn } from "../middlewares/authMiddleware.js";
+import  upload  from "../middlewares/multerMiddleware.js";
+
+
 const router =Router();
 
 
-router.post('/register',register);
+///user module done
+
+ 
+
+
+
+
+router.post('/register', upload.single("avatar") ,register);
+
 router.post('/login',login);
 router.get('/logout',logout);
 router.get('/me',isLoggedIn,getProfile);
-
+router.post('/forgot-password',forgotPassword);
+router.post('/reset-password/:resetToken',resetPassword);
+router.post('/change-password',isLoggedIn,changePassword);
+router.put('/update/:id',isLoggedIn,upload.single("avatar"),updateUser);
 
 
 
